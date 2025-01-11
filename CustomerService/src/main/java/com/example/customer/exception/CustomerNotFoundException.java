e17014ad66846575a90f194c2f7fc93f21b0e230
Bhagyashri Dipak Patil <130145847+Bhagyashri1520@users.noreply.github.com>
2025-01-11 16:41:42 +0530

    Delete CustomerService directory

--- CustomerService/src/main/java/com/example/customer/exception/CustomerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.customer.exception;
-
-public class CustomerNotFoundException extends RuntimeException {
-    
-    public CustomerNotFoundException(String message) {
-        super(message); // Pass the custom message to the parent class constructor
-    }
-}
